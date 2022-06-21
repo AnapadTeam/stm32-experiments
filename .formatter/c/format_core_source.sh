1f91ce206b342c79d5c2752332fcf7376d4c475d
Petersoj <Petersoj@users.noreply.github.com>
2022-06-20 19:07:25 -0600

    Add clang formatter shell script

---
+++ .formatter/c/format_core_source.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIRECTORY_TO_FORMAT="Core/"
+
+if [[ ! -d "$DIRECTORY_TO_FORMAT" ]]; then
+    echo "This script must be called from the project directory (e.g. where the \"Core\" directory is)."
+    exit 1
+fi
+
+find "$DIR_TO_FORMAT" -iname *.c -o -iname *.h | xargs clang-format -i --style="{BasedOnStyle: LLVM, ColumnLimit: 120, IndentWidth: 4, IndentCaseLabels: true}"
+
